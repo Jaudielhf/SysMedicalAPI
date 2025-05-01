64a2f1b2f738a54e3686691bf644187b0121bb66
Jaudielhf <jaudielhernandez23@gmail.com>
2025-05-01 13:20:07 -0600

    Añadido DiagnosticoController y servicio DiagnosticoSrv
    - Se configuró `DiagnosticoController` como controlador de API con
      dependencias necesarias y un nuevo endpoint `Lst` para obtener
      diagnósticos.
    - Se creó la clase `DiagnosticoSrv` con lógica para consultar
      diagnósticos desde la base de datos, manejo de excepciones y
      encapsulación de respuestas en `ResponseVM`.

--- SysMedicalAPI/Controllers/DiagnosticoController.cs
+++ SysMedicalAPI/Controllers/DiagnosticoController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SysMedicalAPI.Data;
+using SysMedicalAPI.Services;
+using WebApiCost.ViewModels;
 
 namespace SysMedicalAPI.Controllers
 {
+  [ApiController]
+  [Route("[controller]")]
   public class DiagnosticoController : Controller
   {
+    private readonly SaludEscolarContext _context;
+    private readonly DiagnosticoSrv diagnosticoSrv;
 
+    public DiagnosticoController(SaludEscolarContext context)
+    {
+      _context = context;
+      diagnosticoSrv = new DiagnosticoSrv(context);
+    }
+
+    [HttpGet]
+    [Route("Lst")]
+    public async Task<ActionResult<ResponseVM>> Lst() => await diagnosticoSrv.GetAllDiagnosticById();
   }
 }
---
+++ SysMedicalAPI/Services/DiagnosticoSrv.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SysMedicalAPI.Data;
+using WebApiCost.ViewModels;
+
+namespace SysMedicalAPI.Services
+{
+  public class DiagnosticoSrv
+  {
+    private readonly SaludEscolarContext _context;
+
+    public DiagnosticoSrv(SaludEscolarContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<ResponseVM> GetAllDiagnosticById()
+    {
+      var response = new ResponseVM();
+      try
+      {
+        var diagnostics = await _context.Diagnosticos.ToListAsync();
+        if (diagnostics == null || diagnostics.Count == 0)
+        {
+          response.Error("No existen datos");
+        }
+        else
+        {
+          response.Find(diagnostics?.Count > 0);
+          response.Data = diagnostics;
+        }
+      }
+      catch (SqlException ex)
+      {
+        response.Error($"Error en la base de datos: {ex.Message}");
+      }
+      catch (DbUpdateException ex)
+      {
+        response.Error($"Error actualizando datos: {ex.Message}");
+      }
+      catch (Exception ex)
+      {
+        response.Error($"Error interno del servidor: {ex.Message}");
+      }
+
+      return response; // Ensure a value is returned in all code paths  
+    }
+
+  }
+}
